c99179fe7ac1eec1ed24b05082283ce562bccd12
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2020-03-15 17:53:15 -0400

    Added Home and About Routes

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About This GitHub Finder Application</h1>
+      <p>
+        This is a simple, web application to find GitHub users using the GitHub
+        API.
+      </p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
